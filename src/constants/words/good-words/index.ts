b66679746fc9a65ed7fc44632fe693635594609f
Ryan M <59982450+Ryanawesome111@users.noreply.github.com>
2023-08-27 13:13:07 -0400

    Update index.ts

--- src/constants/words/good-words/index.ts
+++ src/constants/words/good-words/index.ts
@@ -2,4 +2,4 @@ import { words as seventh } from "./seventh";
 import { words as eight } from "./eight";
 import { words as sixth } from "./sixth";
 
-export const words = new Set(Two.concat(seventh).concat(eight).concat(sixth));
+export const words = new Set(Two.concat(eight));
